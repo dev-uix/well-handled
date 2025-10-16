995d28dff1b2f4aae138ded7abafde8ee16fba5c
dev-uix <lexparkinson@gmail.com>
2025-10-16 11:27:54 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,30 @@ window.addEventListener('scroll', () => {
   // Set the background-position Y based on scroll offset
   parallax.style.backgroundPosition = `center ${-offset}px`;
 });
+
+
+document.querySelectorAll('.accordion-header').forEach(button => {
+  button.addEventListener('click', () => {
+    const accordionContent = button.nextElementSibling;
+
+    button.classList.toggle('active');
+
+    if (button.classList.contains('active')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      accordionContent.classList.add('open');
+    } else {
+      accordionContent.style.maxHeight = 0;
+      accordionContent.classList.remove('open');
+    }
+
+    // Close other accordions
+    document.querySelectorAll('.accordion-header').forEach(otherButton => {
+      if (otherButton !== button && otherButton.classList.contains('active')) {
+        otherButton.classList.remove('active');
+        const otherContent = otherButton.nextElementSibling;
+        otherContent.style.maxHeight = 0;
+        otherContent.classList.remove('open');
+      }
+    });
+  });
+});
